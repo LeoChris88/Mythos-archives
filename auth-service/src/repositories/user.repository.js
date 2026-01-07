faaf1524ff34f90dde99ae61712544044213a6df
LeoChris88 <leochan2005@outlook.fr>
2026-01-07 14:49:25 +0100

    debbug version Prisma

--- auth-service/src/repositories/user.repository.js
+++ auth-service/src/repositories/user.repository.js
@@ -1,10 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient(); // rien d’autre à mettre ici
 
-module.exports = {
-  createUser: (data) => prisma.user.create({ data }),
-  findByEmail: (email) => prisma.user.findUnique({ where: { email } }),
-  findById: (id) => prisma.user.findUnique({ where: { id } }),
-  updateRole: (id, role) => prisma.user.update({ where: { id }, data: { role } }),
-  getAllUsers: () => prisma.user.findMany(),
-};
+const prisma = new PrismaClient();
+
+module.exports = prisma;
