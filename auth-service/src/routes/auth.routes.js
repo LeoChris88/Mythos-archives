326735e7ab7fcd1e563e6c0b9509c55b9692bda9
LeoChris88 <leochan2005@outlook.fr>
2026-01-10 14:11:58 +0100

    début 13/20 + requêtes créations de témoignages

--- auth-service/src/routes/auth.routes.js
+++ auth-service/src/routes/auth.routes.js
@@ -4,11 +4,9 @@ const authController = require("../controllers/auth.controller");
 const { authMiddleware } = require("../middlewares/auth.middleware");
 const { isAdmin } = require("../middlewares/role.middleware");
 
-// Public routes
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
-// Protected routes
 router.get("/me", authMiddleware, authController.me);
 router.get("/admin/users", authMiddleware, isAdmin, authController.listUsers);
 router.patch("/users/:id/role", authMiddleware, isAdmin, authController.changeRole);
